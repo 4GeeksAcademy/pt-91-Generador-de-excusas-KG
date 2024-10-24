0bc0aae923ae022fe99b76a92fea88b0d2d02c22
Karoll Guzmán <karolldguzman@gmail.com>
2024-10-24 20:36:21 +0000

    Refactorizar código para generar excusas aleatorias

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseElement = document.getElementById("excusa");
+  excuseElement.textContent = excuseGenerator();
 };
+
+const whoToBlame = ["The dog", "My mom", "The cat", "The bird", "The teacher"];
+const whatTheyDid = ["ate", "stole", "hid", "destroyed", "burned"];
+const whatDoYouWantToAvoid = [
+  "my homework",
+  "my phone",
+  "my shoes",
+  "my book",
+  "my video game"
+];
+const when = [
+  "yesterday",
+  "last night",
+  "this morning",
+  "last week",
+  "last month"
+];
+
+function excuseGenerator() {
+  const randomWho = Math.floor(Math.random() * whoToBlame.length);
+  const randomWhat = Math.floor(Math.random() * whatTheyDid.length);
+  const randomWhatToAvoid = Math.floor(
+    Math.random() * whatDoYouWantToAvoid.length
+  );
+  const randomWhen = Math.floor(Math.random() * when.length);
+
+  return (
+    whoToBlame[randomWho] +
+    " " +
+    whatTheyDid[randomWhat] +
+    " " +
+    whatDoYouWantToAvoid[randomWhatToAvoid] +
+    " " +
+    when[randomWhen]
+  );
+}
+console.log(excuseGenerator());

c5a68e60969698977c30129f0fea50dc68b0f920
Karoll Guzmán <karolldguzman@gmail.com>
2024-10-25 17:06:57 +0000

    Refactorizar código para mejorar generación de excusas aleatorias y eliminar líneas innecesarias

--- src/app.js
+++ src/app.js
@@ -7,32 +7,22 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 
   function generateExcuse() {
-    const who = ["The dog", "My grandma", "His turtle", "My bird"];
-    const action = ["ate", "peed", "crushed", "broke"];
-    const what = ["my homework", "the keys", "the car", "my phone"];
-    const when = ["before the class", "right on", "this morning", "last week"];
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-    const whoIndex = Math.floor(Math.random() * who.length);
-    const actionIndex = Math.floor(Math.random() * action.length);
-    const whatIndex = Math.floor(Math.random() * what.length);
-    const whenIndex = Math.floor(Math.random() * when.length);
-    console.log(
-      who[whoIndex] +
-        " " +
-        action[actionIndex] +
-        " " +
-        what[whatIndex] +
-        " " +
-        when[whenIndex]
-    );
-    return (
-      who[whoIndex] +
-      " " +
-      action[actionIndex] +
-      " " +
-      what[whatIndex] +
-      " " +
-      when[whenIndex]
-    );
+    let randomWho = Math.floor(Math.random() * who.length);
+    let randomAction = Math.floor(Math.random() * action.length);
+    let randomWhat = Math.floor(Math.random() * what.length);
+    let randomWhen = Math.floor(Math.random() * when.length);
+
+    return randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
   }
 });

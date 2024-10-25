3bf82a14854ffc8b91effa8e3ae1318ce618ed88
Karoll Guzmán <karolldguzman@gmail.com>
2024-10-25 00:31:06 +0000

    Refactorizar código para mejorar generación de excusas aleatorias y eliminar código innecesari

--- src/app.js
+++ src/app.js
@@ -1,38 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const excuseParagraph = document.getElementById("excusa");
+function generateExcuse() {
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car", "my phone"];
+  const when = ["before the class", "right on", "this morning", "last week"];
 
-  window.excuseGenerator = function() {
-    const excuse = generateExcuse();
-    excuseParagraph.textContent = excuse;
-  };
-
-  function generateExcuse() {
-    const who = ["The dog", "My grandma", "His turtle", "My bird"];
-    const action = ["ate", "peed", "crushed", "broke"];
-    const what = ["my homework", "the keys", "the car", "my phone"];
-    const when = ["before the class", "right on", "this morning", "last week"];
-
-    const whoIndex = Math.floor(Math.random() * who.length);
-    const actionIndex = Math.floor(Math.random() * action.length);
-    const whatIndex = Math.floor(Math.random() * what.length);
-    const whenIndex = Math.floor(Math.random() * when.length);
-    console.log(
-      who[whoIndex] +
-        " " +
-        action[actionIndex] +
-        " " +
-        what[whatIndex] +
-        " " +
-        when[whenIndex]
-    );
-    return (
-      who[whoIndex] +
+  const whoIndex = Math.floor(Math.random() * who.length);
+  const actionIndex = Math.floor(Math.random() * action.length);
+  const whatIndex = Math.floor(Math.random() * what.length);
+  const whenIndex = Math.floor(Math.random() * when.length);
+  console.log(
+    who[whoIndex] +
       " " +
       action[actionIndex] +
       " " +
       what[whatIndex] +
       " " +
       when[whenIndex]
-    );
-  }
-});
+  );
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
